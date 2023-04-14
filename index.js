e40d94aa5db87547bbcf8fd819794904601b5017
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-15 04:39:26 +0530

    configured MongoDB connection

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ const app = express();
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(err))
 
 
-app.listen(8800, ()=>{
+
+app.listen(8800, () => {
     console.log("Backend server is running")
 })
