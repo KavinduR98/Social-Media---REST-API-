e7a11fc5021ac84cb19549d5eb6171ab8efbbed6
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-16 16:55:57 +0530

    create get user API

--- routes/users.js
+++ routes/users.js
@@ -41,5 +41,16 @@ router.delete("/:id", async (req, res) => {
 });
 
 
+//get a user
+router.get("/:id", async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      const { password, updatedAt, ...other } = user._doc;
+      res.status(200).json(other);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
