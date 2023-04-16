9e7d58e792d89d331e90a2667cc3e40a8c1dfde7
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-16 20:35:35 +0530

    update timeline post API

--- routes/posts.js
+++ routes/posts.js
@@ -72,7 +72,6 @@ router.get("/:id", async (req, res) => {
 
 //get all timeline posts
 router.get("/timeline/all", async (req, res) => {
-    let postArray = [];
     try {
         const currentUser = await User.findById(req.body.userId);
         const userPosts = await Post.find({ userId: currentUser._id });
