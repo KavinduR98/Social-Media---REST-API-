136cc8588e002f523bd46f55a807204f3d9f4a0b
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-16 20:11:48 +0530

    create delete post API

--- routes/posts.js
+++ routes/posts.js
@@ -13,16 +13,31 @@ router.post("/", async (req, res) => {
     }
   });
 
-  //update a post
-
+//update a post
 router.put("/:id", async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
       if (post.userId === req.body.userId) {
         await post.updateOne({ $set: req.body });
-        res.status(200).json("the post has been updated");
+        res.status(200).json("The post has been updated");
       } else {
-        res.status(403).json("you can update only your post");
+        res.status(403).json("You can update only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+//delete a post
+router.delete("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.deleteOne();
+        res.status(200).json("The post has been deleted");
+      } else {
+        res.status(403).json("You can delete only your post");
       }
     } catch (err) {
       res.status(500).json(err);
