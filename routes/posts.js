38ea891557e78432465a9fb2fbbb1078665aa39a
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-16 20:29:12 +0530

    create get all posts API

--- routes/posts.js
+++ routes/posts.js
@@ -60,4 +60,30 @@ router.put("/:id/like", async (req, res) => {
     }
   });
 
+//get a post
+router.get("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//get all timeline posts
+router.get("/timeline/all", async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.body.userId);
+      const userPosts = await Post.find({ userId: currentUser._id });
+      const friendPosts = await Promise.all(
+        currentUser.followings.map((friendId) => {
+          return Post.find({ userId: friendId });
+        })
+      );
+      res.json(userPosts.concat(...friendPosts))
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
   module.exports = router;
