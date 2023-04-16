d0bc4220f1c18ada418c60f383186beb7b33caa0
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-16 20:02:56 +0530

    create update post API

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 const app = express();
 
@@ -26,8 +27,9 @@ app.use(morgan("common"));
 // Routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 
 app.listen(8800, () => {
     console.log("Backend server is running")
-})
+});
--- routes/posts.js
+++ routes/posts.js
@@ -12,3 +12,21 @@ router.post("/", async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  //update a post
+
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("the post has been updated");
+      } else {
+        res.status(403).json("you can update only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
